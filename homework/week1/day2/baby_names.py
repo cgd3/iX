eb176c132cdf8fbc3e284dfa11807ce997ccea34
cgd3 <cgd3@princeton.edu>
2018-06-27 11:24:29 +0100

    baby names updated

--- homework/week1/day2/baby_names.py
+++ homework/week1/day2/baby_names.py
@@ -14,10 +14,42 @@ most popular baby names for boys and girls for a certain year (the year is in th
 """
 
 ##need to create file with first line of each txt file 
-n = 2017 - 1900
-for i in range(n):
-    #print(names_{}.format())
 
-#with open('data/names_1900.txt') as f:
-#    first_line = f.readline()
-#    print(first_line)
+from collections import Counter 
+
+def load_names():
+       
+    boy_names = []
+    girl_names = []
+    
+    #read in data 
+    years = range(1900, 2017)
+    for i in years:
+        with open('data/names_{}.txt'.format(i)) as f:
+            first_line = f.readline().strip('\n')
+            
+            boy_names.append((first_line.split('|')[1]))
+            girl_names.append((first_line.split('|')[2]))
+            #print(boy_names)
+            #print(girl_names)
+   
+    #create dictionaries to count names 
+    boy_dictionary = Counter(boy_names)
+    girl_dictionary =Counter(girl_names)
+    print(boy_dictionary)
+    print(girl_dictionary)
+    
+    #accept name as argument 
+    name_input = input("Insert your name").capitalize()
+    
+    if name_input not in (girl_dictionary and boy_dictionary):
+        print("This name was most popular 0 times for boys and girls")
+    elif name_input in girl_dictionary:
+        print("This name was most popular {} time for girls and 0 times for boys".format(girl_dictionary[name_input]))
+    elif name_input in boy_dictionary:
+        print("This name was most popular 0 time for girls and {} times for boys".format(boy_dictionary[name_input]))
+    
+
+load_names()
+            
+            
